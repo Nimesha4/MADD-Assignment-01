0bdafa569865b7079c205bd35e5a30d0086efde0
Nimesha Jayawickrama <nimeshakavindhi4@gmail.com>
2026-06-07 14:35:37 +0530

    testing

---
+++ NurseryAppUITests./NurseryAppUITests_.swift
@@ -0,0 +1,16 @@
+//
+//  NurseryAppUITests_.swift
+//  NurseryAppUITests.
+//
+//  Created by Nimesha Jayawickrama on 2026-06-07.
+//
+
+import Testing
+
+struct NurseryAppUITests_ {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
